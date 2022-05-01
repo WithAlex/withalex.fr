8d29eb7d2305f0d58948a440d7656abb8d098026
alexandre <chat@withalex.fr>
2022-05-01 15:24:03 +0200

    coomit

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
-    <div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+    <div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white mx-6 lg:mx-0">
       <div class="p-4 md:p-12 text-center lg:text-left">
         <!-- Image for mobile view-->
-        <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://via.placeholder.com/1080x1620?text=Bient%C3%B4t+moi')"></div>
+        <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://res.cloudinary.com/dxttfb2j5/image/upload/f_auto,q_auto/v1651411132/PSX_20220501_151609_rshd5t.jpg')"></div>
 
         <h1 class="text-3xl font-bold pt-8 lg:pt-0">LESAGE Alexandre</h1>
         <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
@@ -34,7 +34,7 @@
 
     <!--Img Col-->
     <div class="w-full lg:w-2/5">
-      <img src="https://via.placeholder.com/1080x1620?text=Bient%C3%B4t+moi" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" alt="Moi">
+      <img src="https://res.cloudinary.com/dxttfb2j5/image/upload/f_auto,q_auto/v1651411132/PSX_20220501_151609_rshd5t.jpg" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" alt="Moi">
     </div>
   </div>
 </template>
